a704469035d52980c8a9e84ff8c04ec27ab68430
yeverchan <75665160+yeverchan@users.noreply.github.com>
2022-05-27 21:21:29 +0900

    Refactor: 멤버 서비스 생성자 리팩토링

--- member-service/src/main/java/com/whatalk/memberservice/service/MemberServiceImpl.java
+++ member-service/src/main/java/com/whatalk/memberservice/service/MemberServiceImpl.java
@@ -2,19 +2,17 @@ package com.whatalk.memberservice.service;
 
 import com.whatalk.memberservice.domain.Member;
 import com.whatalk.memberservice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public Optional<Member> findByEmail(String email) {

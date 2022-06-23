89b948a1113858955d26db75b02855c8854f537a
yeverchan <75665160+yeverchan@users.noreply.github.com>
2022-06-23 20:46:13 +0900

    Feat: AuthenticationProvider 추가

---
+++ member-service/src/main/java/com/whatalk/memberservice/auth/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,44 @@
+package com.whatalk.memberservice.auth.provider;
+
+import com.whatalk.memberservice.service.MemberService;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final MemberService memberService;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(MemberService memberService, BCryptPasswordEncoder passwordEncoder) {
+        this.memberService = memberService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String email = (String) authentication.getCredentials();
+        String password = (String) authentication.getPrincipal();
+
+        UserDetails userDetails = memberService.loadUserByUsername(email);
+
+        if(!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("비밀번호가 다릅니다.");
+        }
+
+        return new UsernamePasswordAuthenticationToken(email, password, new ArrayList<>());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}

094f9193e0f6379411490c1ad14fdbcdc782c7b6
yeverchan <75665160+yeverchan@users.noreply.github.com>
2022-06-24 21:06:52 +0900

    Feat: authenticationManager 추가

--- member-service/src/main/java/com/whatalk/memberservice/auth/config/SecurityConfig.java
+++ member-service/src/main/java/com/whatalk/memberservice/auth/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import com.whatalk.memberservice.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +21,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final MemberService memberService;
+    private final Environment environment;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,27 +31,25 @@ public class SecurityConfig {
         http.
                 csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/members").anonymous()
-                .anyRequest().authenticated();
-
-
+//                .antMatchers("/members").anonymous()
+//                .anyRequest().authenticated();
+                .anyRequest().permitAll();
         return http.build();
     }
 
     @Bean
-    public CustomAuthenticationFilter customAuthenticationFilter() {
-        // TODO: 2022/06/23  authenticationManager bean 등록
-        return new CustomAuthenticationFilter("/login", authenticationManager());
-    }
-
-    @Bean
     public CustomAuthenticationProvider customAuthenticationProvider(){
-        return new CustomAuthenticationProvider(memberService, passwordEncoder());
+        return new CustomAuthenticationProvider(memberService, passwordEncoder);
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(customAuthenticationProvider());
+    }
+
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter() {
+        return new CustomAuthenticationFilter("/login", authenticationManager(), environment);
     }
 
 }
